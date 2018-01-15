6fe2ca85076464fd0fef8d7f486cb98cb279346d
hdvinh361989 <vinhux@gmail.com>
2018-01-15 19:42:56 +0700

    redirect

--- data/document.js
+++ data/document.js
@@ -1,10 +1,4 @@
 ﻿(function () {
-    if ((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i))) {
-        location.replace("http://cleanuxd/cleanuxd_-__mobile_site.html");
-    }else if (screen.width <= 768) {
-        document.location = "cleanuxd_-__mobile_site.html";
-    }
-
    jQuery(document).ready(function () {
        var script = document.createElement('script');
        script.src = '/resources/scripts/hotjar/hotjar.js';
